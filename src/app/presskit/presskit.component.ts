48ed543ae7fb0f740e0913408e8eab5840fb4563
VanhouteghemJ <vanhouteghem.j@gmail.com>
2025-06-22 04:14:12 +0200

    feat(presskit): init template

--- src/app/presskit/presskit.component.ts
+++ src/app/presskit/presskit.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterModule, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-presskit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './presskit.component.html',
   styleUrl: './presskit.component.scss'
 })
